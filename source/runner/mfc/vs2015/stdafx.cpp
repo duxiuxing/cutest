f9a994fc19fb080f19a612d2842661248c1e6c17
杜修杏 <duxiuxing@foxmail.com>
2018-06-19 15:28:32 +0800

    vs2015开发环境下，cutest和TestRunnerApp工程结构优化

--- source/runner/mfc/vs2015/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-﻿
-// stdafx.cpp : source file that includes just the standard includes
-// TestRunnerApp.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-#pragma comment(lib, "cutest.lib")
