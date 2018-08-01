9d832da38e4b9993f1bd9a49535e0b0737c0a2d7
duxiuxing <duxiuxing@foxmail.com>
2018-08-01 12:49:16 +0800

    代码优化

--- source/third_party/cppunit/include/cppunit/ExplicitEndTestCaller.h
+++ source/third_party/cppunit/include/cppunit/ExplicitEndTestCaller.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 
+#include <cppunit/Protector.h>
 #include <cppunit/TestCase.h>
 
 #include "cutest/Event.h"
@@ -19,6 +20,26 @@ class ExplicitEndTestCaller
   , public CPPUNIT_NS::TestCase
 {
   typedef void ( Fixture::*TestMethod )();
+  class MethodFunctor : public CPPUNIT_NS::Functor
+  {
+  public:
+    typedef void ( ExplicitEndTestCaller::*Method )();
+
+    MethodFunctor( ExplicitEndTestCaller *target, Method method )
+      : m_target( target )
+      , m_method( method )
+    {}
+
+    bool operator()() const
+    {
+      ( m_target->*m_method )();
+      return true;
+    }
+
+  private:
+    ExplicitEndTestCaller *m_target;
+    Method m_method;
+  };
 
 public:
   ExplicitEndTestCaller( std::string name, TestMethod test )
@@ -40,34 +61,115 @@ public:
   // 重载TestCase::runTest()
   virtual void runTest() override
   {
-    m_fixtureMethodId = FIXTURE_METHOD_ID_TEST;
-    m_event = CUTEST_NS::Event::createInstance();
-    CUTEST_NS::Runner::instance()->asyncRunOnMainThread( this, false );
-    m_event->wait();
-    m_event->destroy();
-    m_event = NULL;
+    runTestOnMainThread();
+  }
+
+  void runTestOnMainThread()
+  {
+    if ( CUTEST_NS::Runner::currentThreadId() == CUTEST_NS::Runner::mainThreadId() )
+    {
+      if ( m_result )
+        m_result->protect( MethodFunctor( this, &ExplicitEndTestCaller::runTestImmediately ),
+                           this );
+      else
+      {
+        runTestImmediately();
+      }
+    }
+    else
+    {
+      m_fixtureMethodId = FIXTURE_METHOD_ID_TEST;
+      m_event = CUTEST_NS::Event::createInstance();
+      CUTEST_NS::Runner::instance()->asyncRunOnMainThread( this, false );
+      m_event->wait();
+      m_event->destroy();
+      m_event = NULL;
+    }
+  }
+
+  void runTestImmediately()
+  {
+    m_fixture->setEvent( m_event );
+    CUTEST_NS::Runner::instance()->registerExplicitEndTest( m_fixture, timeout_ms );
+    ( m_fixture->*m_test )();
   }
 
   // 重载TestFixture::setUp()
   virtual void setUp() override
   {
-    m_fixtureMethodId = FIXTURE_METHOD_ID_SET_UP;
-    m_event = CUTEST_NS::Event::createInstance();
-    CUTEST_NS::Runner::instance()->asyncRunOnMainThread( this, false );
-    m_event->wait();
-    m_event->destroy();
-    m_event = NULL;
+    setUpOnMainThread();
+  }
+
+  void setUpOnMainThread()
+  {
+    if ( CUTEST_NS::Runner::currentThreadId() == CUTEST_NS::Runner::mainThreadId() )
+    {
+      if ( m_result )
+      {
+        m_result->protect( MethodFunctor( this, &ExplicitEndTestCaller::setUpImmediately ),
+                           this, "setUp() failed" );
+      }
+      else
+      {
+        setUpImmediately();
+      }
+    }
+    else
+    {
+      m_fixtureMethodId = FIXTURE_METHOD_ID_SET_UP;
+      m_event = CUTEST_NS::Event::createInstance();
+      CUTEST_NS::Runner::instance()->asyncRunOnMainThread( this, false );
+      m_event->wait();
+      m_event->destroy();
+      m_event = NULL;
+    }
+  }
+
+  void setUpImmediately()
+  {
+    if ( !m_fixture )
+    {
+      m_fixture = new Fixture;
+    }
+    m_fixture->setUp();
+    m_event->post();
   }
 
   // 重载TestFixture::tearDown()
   virtual void tearDown() override
   {
-    m_fixtureMethodId = FIXTURE_METHOD_ID_TEAR_DOWN;
-    m_event = CUTEST_NS::Event::createInstance();
-    CUTEST_NS::Runner::instance()->asyncRunOnMainThread( this, false );
-    m_event->wait();
-    m_event->destroy();
-    m_event = NULL;
+    tearDownOnMainThread();
+  }
+
+  void tearDownOnMainThread()
+  {
+    if ( CUTEST_NS::Runner::currentThreadId() == CUTEST_NS::Runner::mainThreadId() )
+    {
+      if ( m_result )
+      {
+        m_result->protect( MethodFunctor( this, &ExplicitEndTestCaller::tearDownImmediately ),
+                           this, "tearDown() failed" );
+      }
+      else
+      {
+        tearDownImmediately();
+      }
+    }
+    else
+    {
+      m_fixtureMethodId = FIXTURE_METHOD_ID_TEAR_DOWN;
+      m_event = CUTEST_NS::Event::createInstance();
+      CUTEST_NS::Runner::instance()->asyncRunOnMainThread( this, false );
+      m_event->wait();
+      m_event->destroy();
+      m_event = NULL;
+    }
+  }
+
+  void tearDownImmediately()
+  {
+    m_fixture->tearDown();
+    m_event->post();
   }
 
   // 实现Runnable::run()
@@ -76,27 +178,14 @@ public:
     switch ( m_fixtureMethodId )
     {
     case FIXTURE_METHOD_ID_SET_UP:
-      {
-        if ( !m_fixture )
-        {
-          m_fixture = new Fixture;
-        }
-        m_fixture->setUp();
-        m_event->post();
-      }
+
+      setUpOnMainThread();
       break;
     case FIXTURE_METHOD_ID_TEAR_DOWN:
-      {
-        m_fixture->tearDown();
-        m_event->post();
-      }
+      tearDownOnMainThread();
       break;
     case FIXTURE_METHOD_ID_TEST:
-      {
-        m_fixture->setEvent( m_event );
-		CUTEST_NS::Runner::instance()->registerExplicitEndTest( m_fixture, timeout_ms );
-        ( m_fixture->*m_test )();
-      }
+      runTestOnMainThread();
       break;
     default:
       break;
