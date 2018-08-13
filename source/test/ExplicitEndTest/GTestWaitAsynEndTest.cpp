52b02a215d3a8a802d2b264c0210bfc19a9bbf6a
duxiuxing <duxiuxing@foxmail.com>
2018-08-13 15:54:47 +0800

    示例代码上库：利用Windows消息循环来实现异步回调等待

---
+++ source/test/ExplicitEndTest/GTestWaitAsynEndTest.cpp
@@ -0,0 +1,33 @@
+﻿#include "GTestWaitAsynEndTest.h"
+#include <Windows.h>
+
+GTestWaitAsynEndTest::GTestWaitAsynEndTest()
+  : tick_count_setup(0)
+  , time_up(false) {
+}
+
+void GTestWaitAsynEndTest::SetUp() {
+  this->tick_count_setup = CUTEST_NS::Runner::instance()->tickCount64();
+}
+
+void GTestWaitAsynEndTest::TearDown() {
+  SimpleTimer::instance()->removeCallback(this);
+  unsigned long long ms = CUTEST_NS::Runner::instance()->tickCount64() - this->tick_count_setup;
+  EXPECT_GT(ms, 950);
+  EXPECT_LT(ms, 1200);
+}
+
+void GTestWaitAsynEndTest::onTimeUp() {
+  this->time_up = true;
+}
+
+TEST_F(GTestWaitAsynEndTest, end_test_after_1s) {
+  this->time_up = false;
+  SimpleTimer::instance()->setCallback(1000, this);
+
+  MSG msg = {0};
+  while (!this->time_up && ::GetMessage(&msg, NULL, 0, 0)) {
+    ::TranslateMessage(&msg);
+    ::DispatchMessage(&msg);
+  }
+}
---
+++ source/test/ExplicitEndTest/GTestWaitAsynEndTest.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <gtest/gtest.h>
+#include "SimpleTimer.h"
+
+class GTestWaitAsynEndTest
+  : public testing::Test
+  , public SimpleTimer::Callback {
+ public:
+  GTestWaitAsynEndTest();
+
+  virtual void SetUp() override;
+  virtual void TearDown() override;
+
+  // 实现SimpleTimer::Callback
+  virtual void onTimeUp();
+
+  unsigned long long tick_count_setup;
+  bool time_up;
+};
