e3c5d928081fe890a5c9293c44a6af6a5c30fb8d
杜修杏 <duxiuxing@foxmail.com>
2018-08-30 19:08:44 +0800

    cutest提供公共的main()实现

--- source/third_party/cutest/src/Main.cpp
+++ source/third_party/cutest/src/Main.cpp
@@ -1,5 +1,4 @@
 #include "stdafx.h"
-#include "TestConfig.h"
 
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include "cutest/Runner.h"
@@ -7,7 +6,6 @@
 int _tmain(int argc, _TCHAR* argv[]) {
     ::CoInitialize(NULL);
 
-    TestConfig::GetInstance()->Load();
     CPPUNIT_NS::Test* allTests = CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest();
     CUTEST_NS::Runner::instance()->start(allTests);
     CUTEST_NS::Runner::instance()->waitUntilAllTestEnd();
