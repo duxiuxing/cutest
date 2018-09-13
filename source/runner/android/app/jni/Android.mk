74565a83cd4ef81620b33edd3c0c218c9b849c26
杜修杏 <duxiuxing@foxmail.com>
2018-09-13 10:12:38 +0800

    ndk编译sample3~sample5

--- source/runner/android/app/jni/Android.mk
+++ source/runner/android/app/jni/Android.mk
@@ -6,9 +6,4 @@ TEST_DIR := $(LOCAL_PATH)/../../../../test
 include $(LOCAL_PATH)/../../../../third_party/cutest/jni/Android.mk
 
 # *.so
-# include $(LOCAL_PATH)/../../../../test/jni/test.mk
-include $(TEST_DIR)/money/jni/money.mk
-include $(TEST_DIR)/simple/jni/simple.mk
-include $(TEST_DIR)/ExplicitEndTest/jni/ExplicitEndTest.mk
-include $(TEST_DIR)/sample1/jni/sample1.mk
-include $(TEST_DIR)/sample2/jni/sample2.mk
+include $(TEST_DIR)/jni/test.mk
--- source/test/jni/test.mk
+++ source/test/jni/test.mk
@@ -1,10 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(LOCAL_PATH)/../ExplicitEndTest/jni/ExplicitEndTest.mk
-include $(LOCAL_PATH)/../money/jni/money.mk
-include $(LOCAL_PATH)/../sample1/jni/sample1.mk
-include $(LOCAL_PATH)/../sample2/jni/sample2.mk
-include $(LOCAL_PATH)/../sample3/jni/sample3.mk
-include $(LOCAL_PATH)/../sample4/jni/sample4.mk
-include $(LOCAL_PATH)/../sample5/jni/sample5.mk
-include $(LOCAL_PATH)/../simple/jni/simple.mk
+TEST_DIR := $(LOCAL_PATH)/..
+
+include $(TEST_DIR)/ExplicitEndTest/jni/ExplicitEndTest.mk
+include $(TEST_DIR)/money/jni/money.mk
+include $(TEST_DIR)/sample1/jni/sample1.mk
+include $(TEST_DIR)/sample2/jni/sample2.mk
+include $(TEST_DIR)/sample3/jni/sample3.mk
+include $(TEST_DIR)/sample4/jni/sample4.mk
+include $(TEST_DIR)/sample5/jni/sample5.mk
+include $(TEST_DIR)/simple/jni/simple.mk
